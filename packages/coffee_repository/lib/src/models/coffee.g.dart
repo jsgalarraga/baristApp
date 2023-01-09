ec1328834dc092b42b1380f857a8cec05389d07f
jsgalarraga <jaimesanchez8@gmail.com>
2023-01-09 15:41:02 +0100

    add build yaml in coffee repository

--- packages/coffee_repository/lib/src/models/coffee.g.dart
+++ packages/coffee_repository/lib/src/models/coffee.g.dart
@@ -6,10 +6,18 @@ part of 'coffee.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-Coffee _$CoffeeFromJson(Map<String, dynamic> json) => Coffee(
-      imageUrl: json['imageUrl'] as String,
+Coffee _$CoffeeFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Coffee',
+      json,
+      ($checkedConvert) {
+        final val = Coffee(
+          imageUrl: $checkedConvert('image_url', (v) => v as String),
+        );
+        return val;
+      },
+      fieldKeyMap: const {'imageUrl': 'image_url'},
     );
 
 Map<String, dynamic> _$CoffeeToJson(Coffee instance) => <String, dynamic>{
-      'imageUrl': instance.imageUrl,
+      'image_url': instance.imageUrl,
     };
