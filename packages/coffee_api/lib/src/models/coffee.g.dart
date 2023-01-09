97c739a85dc5b9dc8c64713d5fbe4ec0e6f3d856
jsgalarraga <jaimesanchez8@gmail.com>
2023-01-09 15:39:52 +0100

    add build yaml in coffee api package

--- packages/coffee_api/lib/src/models/coffee.g.dart
+++ packages/coffee_api/lib/src/models/coffee.g.dart
@@ -1,15 +1,20 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: implicit_dynamic_parameter
+
 part of 'coffee.dart';
 
 // **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
-Coffee _$CoffeeFromJson(Map<String, dynamic> json) => Coffee(
-      file: json['file'] as String,
+Coffee _$CoffeeFromJson(Map<String, dynamic> json) => $checkedCreate(
+      'Coffee',
+      json,
+      ($checkedConvert) {
+        final val = Coffee(
+          file: $checkedConvert('file', (v) => v as String),
+        );
+        return val;
+      },
     );
-
-Map<String, dynamic> _$CoffeeToJson(Coffee instance) => <String, dynamic>{
-      'file': instance.file,
-    };
