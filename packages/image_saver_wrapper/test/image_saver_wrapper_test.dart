93604dc63d39c911ec379527a719861e1e2a9e2a
jsgalarraga <jaimesanchez8@gmail.com>
2023-01-10 18:39:54 +0100

    add tests

--- packages/image_saver_wrapper/test/image_saver_wrapper_test.dart
+++ packages/image_saver_wrapper/test/image_saver_wrapper_test.dart
@@ -29,5 +29,25 @@ void main() {
         verify(() => imageSaverRepository.saveImageFromUrl(imageUrl)).called(1);
       });
     });
+
+    group('isPhotosPermissionDenied', () {
+      test('exists', () async {
+        try {
+          await imageSaverRepository.isPhotosPermissionDenied();
+        } catch (_) {}
+        verify(() => imageSaverRepository.isPhotosPermissionDenied()).called(1);
+      });
+    });
+
+    group('isPhotosPermissionPermanentlyDenied', () {
+      test('exists', () async {
+        try {
+          await imageSaverRepository.isPhotosPermissionPermanentlyDenied();
+        } catch (_) {}
+        verify(
+          () => imageSaverRepository.isPhotosPermissionPermanentlyDenied(),
+        ).called(1);
+      });
+    });
   });
 }
