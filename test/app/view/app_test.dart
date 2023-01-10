e7e16cc76edea4bab805235785867f07edd661d6
jsgalarraga <jaimesanchez8@gmail.com>
2023-01-10 18:09:31 +0100

    fix tests

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -2,6 +2,7 @@ import 'package:coffee_app/app/app.dart';
 import 'package:coffee_app/coffee/coffee.dart';
 import 'package:coffee_repository/coffee_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:image_saver_wrapper/image_saver_wrapper.dart';
 
 void main() {
   group('App', () {
@@ -9,6 +10,7 @@ void main() {
       await tester.pumpWidget(
         App(
           coffeeRepository: CoffeeRepository(),
+          imageSaver: ImageSaverWrapper(),
         ),
       );
       expect(find.byType(CoffeePage), findsOneWidget);
