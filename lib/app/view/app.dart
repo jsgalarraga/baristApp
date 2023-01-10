1da24b4f1a8689e8cde7ce4e7cc4c68475cb1a38
jsgalarraga <jaimesanchez8@gmail.com>
2023-01-10 12:50:42 +0100

    update theme

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -3,6 +3,7 @@ import 'package:coffee_app/l10n/l10n.dart';
 import 'package:coffee_repository/coffee_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   const App({
@@ -18,9 +19,20 @@ class App extends StatelessWidget {
       value: _coffeeRepository,
       child: MaterialApp(
         theme: ThemeData(
-          appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-          colorScheme: ColorScheme.fromSwatch(
-            accentColor: const Color(0xFF13B9FF),
+          backgroundColor: const Color(0xffffead5),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xffb58b61),
+            primary: const Color(0xffb58b61),
+          ),
+          textTheme: GoogleFonts.poppinsTextTheme(
+            const TextTheme(
+              bodyMedium: TextStyle(fontSize: 16),
+              headlineLarge: TextStyle(
+                fontSize: 40,
+                height: 1.2,
+                color: Colors.black87,
+              ),
+            ),
           ),
         ),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
